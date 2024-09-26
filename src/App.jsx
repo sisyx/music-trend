ec79434701fa9630c1dc2e4ffc7629b6184d5894
sisyx <bahadorisajad908@gamilc.com>
2024-09-26 14:01:45 +0330

    clean src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,9 @@ import './fonts.css';
 import './App.css';
 import './general.css';
 
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { ThemeProvider } from './contexts/themeContext';
 import Loading from './components/loadings/Loading';
-import { isLoggedin } from './functions';
-// const Logout = React.lazy(() => import('./components/login/Logout'));
-// const Payment = React.lazy(() => import('./pages/payment/Payment'));
-// const Admin = React.lazy(() => import('./pages/admin/Admin'));
-// const InstagramReport = React.lazy(() => import('./pages/report/InstagramReport'));
-// const TelegramReport = React.lazy(() => import('./pages/report/TelegramReport'));
-// const Contact = React.lazy(() => import('./pages/contact/Contact'));
-// const Start = React.lazy(() => import("./pages/start/Start"));
-// const AboutUsPage = React.lazy(() => import("./pages/other/AboutUsPage"));
-// const Login = React.lazy(() => import('./pages/login/Login'));
-// const Home = React.lazy(() => import('./pages/home/Home'));
-// import Home from "./pages/home/Home"
-// import Admin from "./pages/admin/Admin"
-// import Login from "./pages/login/Login"
-// import Logout from './components/login/Logout';
-// import AboutUsPage from './pages/other/AboutUsPage'
-// import Start from "./pages/start/Start"
-// import InstagramReport from './pages/report/InstagramReport'
-// import Contact from './pages/contact/Contact'
-// import Payment from './pages/payment/Payment'
-
 
 const Home = React.lazy(() => import("./pages/home/Home"));
 const Admin = React.lazy(() => import("./pages/admin/Admin"));
@@ -39,9 +17,7 @@ const Contact = React.lazy(() => import('./pages/contact/Contact'));
 const Payment = React.lazy(() => import('./pages/payment/Payment'));
 
 function App() {
-
   return (
-    // <ThemeProvider>
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route index exact element={<Home />} />
@@ -56,7 +32,6 @@ function App() {
             <Route path='/payment' element={<Payment />} />
           </Routes>
       </Suspense>
-    // </ThemeProvider>
   )
 }
 
