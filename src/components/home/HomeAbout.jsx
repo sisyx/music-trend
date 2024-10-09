420c19fd56c6ea31b3b7383809885a53876f3d86
sisyx <bahadorisajad908@gamilc.com>
2024-10-09 15:09:55 +0330

    center 3dCards

--- src/components/home/HomeAbout.jsx
+++ src/components/home/HomeAbout.jsx
@@ -1,5 +1,4 @@
 import { shortAboutus, threefeatures } from "../../texts-source";
-import AnimatedBackground2 from "../AnimatedBackground2";
 import GlassyButton from "../GlassyButton";
 import NeonLightBg from "../NeonLightBg";
 import X3DButton from "../X3DButton";
@@ -9,24 +8,24 @@ function HomeAbout() {
         // outer container
         <div className="relative flex justify-center w-screen">
             <NeonLightBg />
-            <div className="flex md:flex-row flex-col-reverse gap-10 items-center justify-evenly  w-full max-w-7xl py-32">
+            <div className="flex md:flex-row flex-wrap flex-col-reverse gap-x-10 items-center justify-evenly  w-full max-w-7xl py-32">
+                    {/* buttons */}
+                    <div className="w-full flex justify-center">
+                        <div className="flex md:gap-8 flex-wrap p-2 gap-2 mb-4 overflow-hidden">
+                            {
+                                threefeatures.map(feature =>
+                                    <X3DButton>
+                                        <div className="flex flex-col items-center" dir="ltr">
+                                            <span className="text-3xl text-telegram font-extrabold">{feature.value}</span>
+                                            <span className=" text-xs text-gray-600">{feature.text}</span>
+                                        </div>
+                                    </X3DButton>
+                                )
+                                }
+                        </div>
+                    </div>
                 {/* LEFT */}
                 <div className="flex-1" dir="rtl">
-                    
-                    {/* buttons */}
-                    <div className="flex md:gap-8 flex-wrap p-2 gap-2 mb-12 overflow-hidden">
-                        {
-                            threefeatures.map(feature =>
-                                <X3DButton>
-                                    <div className="flex flex-col items-center" dir="ltr">
-                                        <span className="text-3xl text-telegram font-extrabold">{feature.value}</span>
-                                        <span className=" text-xs text-gray-600">{feature.text}</span>
-                                    </div>
-                                </X3DButton>
-                             )
-                        }
-                    </div>
-
                     {/* text and detail */}
                     <div className=" leading-8 p-2 md:p-0 text-justify text-sm font-extralight flex flex-col gap-4">
                         <div>
