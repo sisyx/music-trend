046fb8c157621acb0cf7fc7244efeb905afd07ae
sisyx <bahadorisajad908@gmail.com>
2024-12-10 18:39:37 +0330

    update: Enamad link

--- src/components/ENamad.jsx
+++ src/components/ENamad.jsx
@@ -1,7 +1,7 @@
 function ENamad() {
     return ( 
-        <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=552550&Code=ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle">
-            <img referrerpolicy="origin" src="https://trustseal.enamad.ir/logo.aspx?id=552550&Code=ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle" alt="" style={{cursor:"pointer"}} code="ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle" />
+        <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=552550&Code=ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle'>
+            <img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=552550&Code=ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle' alt='' style='cursor:pointer' code='ZgdGfyGicSZ0JirSy4dIjL4hqBNr7cle' />
         </a>
      );
 }
